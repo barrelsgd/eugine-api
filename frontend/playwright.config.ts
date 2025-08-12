7750fbd215246c6bbec37e14d5ab4fca104e5ddb
Eugine Whint <euginegnd@gmail.com>
2025-08-12 15:26:56 +1200

    'test

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -90,7 +90,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run dev',
-    url: 'http://localhost:5173',
+    url: 'http://localhost:4173',
     reuseExistingServer: !process.env.CI,
   },
 });
